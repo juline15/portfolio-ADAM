3d91182d3f263b8d5f5815938621e55ec886786b
Juline Adam <juline.adam@edu.univ-fcomte.fr>
2025-11-13 09:04:25 +0100

    test deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import node from '@astrojs/node'; // Importer l'adaptateur
 
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
+    output: 'server', // Passer en mode SSR
+    adapter: node({ // Ajouter l'adaptateur Node.js
+        mode: 'standalone'
+    }),
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
